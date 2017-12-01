8df156bad4df374110848bcb2ad9d78d397642f9
ZEvans1 <zte.zachary@gmail.com>
2017-12-01 09:47:09 -0800

    Edit html, add Javascript that will store and alert captured variables.

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    var question2 = $("input#q2").val();
+    var question3 = $("input:radio[name=option]:checked").val();
+    var question4 = $("#q4").val();
+    var question5 = $("#q5").val();
+
+    alert(question2);
+    alert(question3);
+    alert(question5);
+
+    event.preventDefault();
+  });
+});
+
+
+
+//     $(".name").text(nameInput);
+//     $(".address").text(addressInput);
+//     $(".item").text(itemInput);
+//     $(".ship").text(shipInput);
+//
+//     $("#formOne").hide();
+//     $("#reciept").show();
+
+
+
+// $(document).ready(function() {
+//   $("form#quiz").submit(function(event) {
+//     var age = parseInt($("input#age").val());
+//     var tv = $("input#tv").val();
+//     var worth = $("select#rich").val();
+//     var food = $("select#food").val();
+//
+//     if (age <= 30 && tv ==="yes" && worth ==="rich" && food ==="pizza") {
+//         $("#match").show();
+//         $(".trump").show();
+//         $(".putin").hide();
+//       } else if (age >30 && tv ==="no" && food ==="food") {
+//         $("#match").show();
+//         $(".trump").hide();
+//         $(".putin").show();
+//       }
+//       else {
+//       alert('Please enter your age.');
+//     }
