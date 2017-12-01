fd2723a1f3e89ef25f863e412614441768a32ede
ZEvans1 <zte.zachary@gmail.com>
2017-12-01 11:39:08 -0800

    Update README.md and edit Javascript indentation / spacing.

---
+++ README.md
@@ -0,0 +1,35 @@
+# _Epicdous Track Suggester_
+
+#### _An track suggestion page for week 2 @ Epicodus, Dec. 1, 2017_
+
+#### By _**Zach Evans**_
+
+## Description
+
+_This is a page that suggests an Epicodus track to the user after filling out a quiz._
+
+## Setup/Installation Requirements
+
+* _This project can be accessed [here](https://github.com/ZEvans1/track-suggester.git)_
+* _Clone the repository to your machine_
+* _Open in a web browser (was made with Chrome)_
+
+## Known Bugs
+
+_There are no known bugs_
+
+## Support and contact details
+
+_Any questions should be directed to zte.zachary@gmail.com_
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _Bootstrap_
+* _Javascript_
+* _JQuery_
+
+### License
+
+Not necessary
--- js/scripts.js
+++ js/scripts.js
@@ -10,41 +10,41 @@ $(document).ready(function() {
       $("#suggestion").hide();
       $("#recommendation").show();
     } else if ((question2 === "yes" && question3 !== "response2" && question3 !== "response3" && question3 !== "response4") || question3 === "response1") {
-      var track = "C#/.NET";
-      $("#suggestion").hide();
-      $("#exception").hide();
-      $("#recommendation").hide();
-      $("#suggestion").show();
-      $("#result").text(track);
+        var track = "C#/.NET";
+        $("#suggestion").hide();
+        $("#exception").hide();
+        $("#recommendation").hide();
+        $("#suggestion").show();
+        $("#result").text(track);
     } else if (question3 === "response2") {
-      var track = "CSS/Design";
-      $("#suggestion").hide();
-      $("#exception").hide();
-      $("#recommendation").hide();
-      $("#suggestion").show();
-      $("#result").text(track);
+        var track = "CSS/Design";
+        $("#suggestion").hide();
+        $("#exception").hide();
+        $("#recommendation").hide();
+        $("#suggestion").show();
+        $("#result").text(track);
     } else if ((question3 === "response3" && question5 === "several") || question5 ==="several") {
-      var track = "Java/Android";
-      $("#suggestion").hide();
-      $("#exception").hide();
-      $("#recommendation").hide();
-      $("#suggestion").show();
-      $("#result").text(track);
+        var track = "Java/Android";
+        $("#suggestion").hide();
+        $("#exception").hide();
+        $("#recommendation").hide();
+        $("#suggestion").show();
+        $("#result").text(track);
     } else if (question3 === "response4") {
-      var track = "Ruby/Rails";
-      $("#suggestion").hide();
-      $("#exception").hide();
-      $("#recommendation").hide();
-      $("#suggestion").show();
-      $("#result").text(track);
+        var track = "Ruby/Rails";
+        $("#suggestion").hide();
+        $("#exception").hide();
+        $("#recommendation").hide();
+        $("#suggestion").show();
+        $("#result").text(track);
     } else if (question3 === "response3" && question5 === "none") {
-      var track = "Java/Android";
-      $("#suggestion").hide();
-      $("#exception").hide();
-      $("#recommendation").hide();
-      $("#suggestion").show();
-      $("#exception").show();
-      $("#result").text(track);
+        var track = "Java/Android";
+        $("#suggestion").hide();
+        $("#exception").hide();
+        $("#recommendation").hide();
+        $("#suggestion").show();
+        $("#exception").show();
+        $("#result").text(track);
     }
     else {
     alert("I couldn't find your match!");
@@ -57,35 +57,3 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
-
-
-
-//     $(".name").text(nameInput);
-//     $(".address").text(addressInput);
-//     $(".item").text(itemInput);
-//     $(".ship").text(shipInput);
-//
-//     $("#formOne").hide();
-//     $("#reciept").show();
-
-
-
-// $(document).ready(function() {
-//   $("form#quiz").submit(function(event) {
-//     var age = parseInt($("input#age").val());
-//     var tv = $("input#tv").val();
-//     var worth = $("select#rich").val();
-//     var food = $("select#food").val();
-//
-//     if (age <= 30 && tv ==="yes" && worth ==="rich" && food ==="pizza") {
-//         $("#match").show();
-//         $(".trump").show();
-//         $(".putin").hide();
-//       } else if (age >30 && tv ==="no" && food ==="food") {
-//         $("#match").show();
-//         $(".trump").hide();
-//         $(".putin").show();
-//       }
-//       else {
-//       alert('Please enter your age.');
-//     }
