3e3a61436d852f98b83d9047feb544cf352df803
ZEvans1 <zte.zachary@gmail.com>
2017-12-01 14:36:34 -0800

    Add more styles, add a name input to form, update Javascript to include user name into result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
+    var name = $("input#user").val();
     var question2 = $("input#q2").val();
     var question3 = $("input:radio[name=option]:checked").val();
     var question4 = $("#q4").val();
@@ -11,40 +12,55 @@ $(document).ready(function() {
       $("#recommendation").show();
     } else if ((question2 === "yes" && question3 !== "response2" && question3 !== "response3" && question3 !== "response4") || question3 === "response1") {
         var track = "C#/.NET";
+        $("#name").hide()
+        $("#name").show()
         $("#suggestion").hide();
         $("#exception").hide();
         $("#recommendation").hide();
         $("#suggestion").show();
         $("#result").text(track);
+        $("#name").text(name);
     } else if (question3 === "response2") {
         var track = "CSS/Design";
+        $("#name").hide()
+        $("#name").show()
         $("#suggestion").hide();
         $("#exception").hide();
         $("#recommendation").hide();
         $("#suggestion").show();
         $("#result").text(track);
+        $("#name").text(name);
     } else if ((question3 === "response3" && question5 === "several") || question5 ==="several") {
         var track = "Java/Android";
+        $("#name").hide()
+        $("#name").show()
         $("#suggestion").hide();
         $("#exception").hide();
         $("#recommendation").hide();
         $("#suggestion").show();
         $("#result").text(track);
+        $("#name").text(name);
     } else if (question3 === "response4") {
         var track = "Ruby/Rails";
+        $("#name").hide()
+        $("#name").show()
         $("#suggestion").hide();
         $("#exception").hide();
         $("#recommendation").hide();
         $("#suggestion").show();
         $("#result").text(track);
+        $("#name").text(name);
     } else if (question3 === "response3" && question5 === "none") {
         var track = "Java/Android";
+        $("#name").hide()
+        $("#name").show()
         $("#suggestion").hide();
         $("#exception").hide();
         $("#recommendation").hide();
         $("#suggestion").show();
         $("#exception").show();
         $("#result").text(track);
+        $("#name").text(name);
     }
     else {
     alert("I couldn't find your match!");
