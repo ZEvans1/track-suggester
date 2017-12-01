2275e57330d39ec5edfa8347f401a67e4fec37c4
ZEvans1 <zte.zachary@gmail.com>
2017-12-01 11:23:36 -0800

    Add changes to HTML, CSS, and Javascript that will enable an appropriate result for all quiz answer combinations.

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,49 @@ $(document).ready(function() {
     var question5 = $("#q5").val();
     var track;
 
-    if (question2 === "yes" || question3 === "response1") {
-            var track = "C#/.NET";
-            $("#suggestion").hide();
-            $("#suggestion").show();
-            $("#result").text(track);
-          } else if (question3 === "response2") {
-            var track = "CSS/Design";
-            $("#suggestion").hide();
-            $("#suggestion").show();
-            $("#result").text(track);
-          }
-          else {
-          alert('Please enter your age.');
-        }
+    if (question4 !== "loveit") {
+      $("#suggestion").hide();
+      $("#recommendation").show();
+    } else if ((question2 === "yes" && question3 !== "response2" && question3 !== "response3" && question3 !== "response4") || question3 === "response1") {
+      var track = "C#/.NET";
+      $("#suggestion").hide();
+      $("#exception").hide();
+      $("#recommendation").hide();
+      $("#suggestion").show();
+      $("#result").text(track);
+    } else if (question3 === "response2") {
+      var track = "CSS/Design";
+      $("#suggestion").hide();
+      $("#exception").hide();
+      $("#recommendation").hide();
+      $("#suggestion").show();
+      $("#result").text(track);
+    } else if ((question3 === "response3" && question5 === "several") || question5 ==="several") {
+      var track = "Java/Android";
+      $("#suggestion").hide();
+      $("#exception").hide();
+      $("#recommendation").hide();
+      $("#suggestion").show();
+      $("#result").text(track);
+    } else if (question3 === "response4") {
+      var track = "Ruby/Rails";
+      $("#suggestion").hide();
+      $("#exception").hide();
+      $("#recommendation").hide();
+      $("#suggestion").show();
+      $("#result").text(track);
+    } else if (question3 === "response3" && question5 === "none") {
+      var track = "Java/Android";
+      $("#suggestion").hide();
+      $("#exception").hide();
+      $("#recommendation").hide();
+      $("#suggestion").show();
+      $("#exception").show();
+      $("#result").text(track);
+    }
+    else {
+    alert("I couldn't find your match!");
+  }
 
     // alert(question2);
     // alert(question3);
